cd0a28d260100244909534416f99e9aec919379a
Cecilie Johansen <ceciliejohansen@kennethpc.kea.local>
2017-02-23 10:54:51 +0100

    remove modal - modal moved to modal.html

---
+++ restaurant.js
@@ -0,0 +1,67 @@
+// foodtrucks start
+// foodtrucks 1 start
+//
+//$('.first_foodtruck').mouseenter(function () {
+//    $(".first_foodtruck_bg").css("background-image", "url('img/mad/tacos_chilangos.jpg')");
+//});
+//
+//$('.first_foodtruck').mouseleave(function () {
+//    $(".first_foodtruck_bg").css("background-image", "url('img/Food_truck_img/Tacos_chilangos.jpg')");
+//});
+//
+//// foodtrucks 1 slut
+//
+//// foodtrucks 2 start
+//$('.second_foodtruck').mouseenter(function () {
+//    $(".second_foodtruck_bg").css("background-image", "url('img/bangkok_mad1.jpg')");
+//});
+//
+//$('.second_foodtruck').mouseleave(function () {
+//    $(".second_foodtruck_bg").css("background-image", "url('img/Food_truck_img/Bangkok_street_kitchen.jpg')");
+//});
+//
+//// foodtrucks 2 slut
+//
+//// foodtrucks 3 start
+//$('.third_foodtruck').mouseenter(function () {
+//    $(".third_foodtruck_bg").css("background-image", "url('img/mad/choptix.jpg')");
+//});
+//
+//$('.third_foodtruck').mouseleave(function () {
+//    $(".third_foodtruck_bg").css("background-image", "url('img/Food_truck_img/Chopstix.jpg')");
+//});
+//
+//// foodtrucks 3 slut
+//
+//// foodtrucks 4 start
+//$('.fourth_foodtruck').mouseenter(function () {
+//    $(".fourth_foodtruck_bg").css("background-image", "url('img/superbao_mad2.jpg')");
+//});
+//
+//$('.fourth_foodtruck').mouseleave(function () {
+//    $(".fourth_foodtruck_bg").css("background-image", "url('img/Food_truck_img/Super_bao.jpg')");
+//});
+//
+//// foodtrucks 4 slut
+//
+//// foodtrucks 5 start
+//$('.fifth_foodtruck').mouseenter(function () {
+//    $(".fifth_foodtruck_bg").css("background-image", "url('img/mad/schnitzel.jpg')");
+//});
+//
+//$('.fifth_foodtruck').mouseleave(function () {
+//    $(".fifth_foodtruck_bg").css("background-image", "url('img/Food_truck_img/The_schnitzel_company.jpg')");
+//});
+//
+//// foodtrucks 5 slut
+//
+//// foodtrucks 6 start
+//$('.sixth_foodtruck').mouseenter(function () {
+//    $(".sixth_foodtruck_bg").css("background-image", "url('img/mad/nepal.jpg')");
+//});
+//
+//$('.sixth_foodtruck').mouseleave(function () {
+//    $(".sixth_foodtruck_bg").css("background-image", "url('img/Food_truck_img/Everest.jpg')");
+//});
+//
+//// foodtrucks 6 slut
