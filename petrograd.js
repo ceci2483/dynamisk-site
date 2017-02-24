68f8e5a1e62d9d5b86eb80e9c4c816bc70e7d90c
Cecilie Johansen <ceciliejohansen@nd-laptop.kea.local>
2017-02-24 11:14:24 +0100

    filter done

--- petrograd.js
+++ petrograd.js
@@ -7,13 +7,86 @@ function sidenVises() {
 	//læs produktliste
 	$.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
 
+	document.querySelector(".sortering_knapper_vegetar").addEventListener("click", filtererVegater)
+	document.querySelector(".sortering_knapper_ikke_vegetar").addEventListener("click", filtererIkkeVegeter)
+	document.querySelector(".sortering_knapper_tilbud").addEventListener("click", filtererTilbud)
+	document.querySelector(".sortering_knapper_udenAlkohol").addEventListener("click", filtererUdenAlkohol)
+
 }
+
+
+function filtererVegater(event) {
+	console.log("klik på vegetar-filter");
+
+	// find alle ikke-vegatar-produkter
+	var liste = document.querySelectorAll(".produkt:not(.vegetar)")
+
+	// skjul den - tilføj klassen hide
+	liste.forEach(div => div.classList.toggle("hide"));
+
+	event.preventDefault();
+}
+
+function filtererIkkeVegeter(event) {
+	console.log("klik på ikke vegetar-filter");
+
+	// find alle ikke-vegatar-produkter
+	var liste = document.querySelectorAll(".produkt.vegetar")
+
+	// skjul den - tilføj klassen hide
+	liste.forEach(div => div.classList.toggle("hide"));
+
+	event.preventDefault();
+}
+
+
+function filtererTilbud(event) {
+	console.log("klik på tilbud-filter");
+
+	// find alle ikke-vegatar-produkter
+	var liste = document.querySelectorAll(".produkt:not(.tilbud)")
+
+	// skjul den - tilføj klassen hide
+	liste.forEach(div => div.classList.toggle("hide"));
+
+	event.preventDefault();
+}
+
+
+function filtererUdenAlkohol(event) {
+	console.log("klik på ikke alkohol-filter");
+
+	// find alle ikke-vegatar-produkter
+	var liste = document.querySelectorAll(".produkt.alkohol")
+
+	// skjul den - tilføj klassen hide
+	liste.forEach(div => div.classList.toggle("hide"));
+
+	event.preventDefault();
+}
+
+
+
+
+
+
+
 
 
 function visProduktListe(listen) {
 	console.table(listen);
+	//TODO: filterer udsolgt produkter fra..
+	listen = listen.filter(produkt => !produkt.udsolgt);
+	//	listen = listen.filter(fjernUdsolgte);
 	listen.forEach(visProdukt);
 }
+
+
+
+function fjernUdsolgte(produkt) {
+	return !produkt.udsolgt;
+}
+
 
 function visProdukt(produkt) {
 	console.log(produkt);
@@ -49,6 +122,62 @@ function visProdukt(produkt) {
 	}
 
 
+	//tilføj klasser til produkt
+
+	if (produkt.vegetar == true) {
+		klon.querySelector(".produkt").classList.add("vegetar");
+	}
+
+	if (produkt.udsolgt) {
+		klon.querySelector(".produkt").classList.add("udsolgt");
+	}
+
+	if (produkt.rabatsats > 0) {
+		klon.querySelector(".produkt").classList.add("tilbud");
+	}
+
+	if (produkt.alkoholprocent > 0) {
+		klon.querySelector(".produkt").classList.add("alkohol");
+	}
+
+
+
+	// tilføj produkt-id til modelknap
+	klon.querySelector(".modalknap").dataset.produkt = produkt.id;
+
+	// registerer klik på modalknap
+	klon.querySelector(".modalknap").addEventListener("click", modalKnapKlik)
+
 	// append klon til .produkt_liste //
 	document.querySelector(".produktliste").appendChild(klon);
+}
+
+function modalKnapKlik(event) {
+	console.log("knapklik", event);
+
+	// find det produkt id, hvis knap
+	var produktId = event.target.dataset.produkt;
+	console.log("klik på produkt: ", produktId);
+
+	$.getJSON("http://petlatkea.dk/2017/dui/api/product?callback=?", {
+		id: produktId
+	}, visModalProdukt);
+}
+
+
+function visModalProdukt(produkt) {
+	console.log("vis modal for ", produkt);
+
+	// find modal_template - klon den
+	var klon = document.querySelector("#modal_template").content.cloneNode(true);
+
+	// put data i klonen
+	klon.querySelector(".data_navn").innerHTML = produkt.navn;
+
+	// sletter det der stod i model_content
+	document.querySelector(".modal-content").innerHTML = "";
+
+	// append klonen til modal_content
+
+	document.querySelector(".modal-content").appendChild(klon);
 }
